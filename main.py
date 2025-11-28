7d3210f8cbb0557806ae5f42901a13043d224c62
Shakir <shekker32@gmail.com>
2025-11-28 22:58:29 +0300

    A lot of changes, including:
    Added if name == main structure - now main part is in function main()
    
    Changed check-score logic - now it's primary priority while executing
    
    Added new variable for part, where program asks about how many darts was thrown to finish
    
    All variables in main() now with global parameter, to ability to change their values while executing
    
    Added part of logic for 9 dart finish
    
    Other cosmetic changes
    
    This is the final 0.2 version

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # global variables
 
-start_score:    int = 501 # standard game of x01 with 501 points
-darts_thrown:   int = 0
-main_input:     str = "" # score of visit (3 darts) or exit, quit
+start_score:        int = 501 # standard game of x01 with 501 points
+darts_thrown:       int = 0
+darts_to_finish:    int = 0
+main_input:         str = "" # score of visit (3 darts) or exit, quit
 
 # functions
 
@@ -17,36 +18,80 @@ def avg_score(start_score: int = 501, darts_thrown: int = 0) -> float:
 
 greeting()
 
-while True:
+def main():
 
-    try:
-        main_input = input("Enter score: ")
-        if main_input in {"quit", "Quit", "exit", "Exit", "QUIT", "EXIT"}:
+    global start_score, main_input, darts_thrown, darts_to_finish
+
+    while True:
+
+        # check-score logic for each iteration
+
+        if start_score == 0 and darts_thrown == 9:
+            print("\n----- 9 DARTER! -----\n")
             break
-        if int(main_input) in {163, 166, 169, 172, 173, 175, 176, 178, 179} or int(main_input) > 180:
-            print(f"\n\n----- Not valid input -----\n\nEnter a valid score\n\n----------\n")
+        if start_score == 0 and darts_thrown > 9:
+            try:
+                darts_to_finish = int(input("\nDarts needed to finish: "))
+                if darts_to_finish > 3:
+                    raise Exception
+                else:
+                    print(f"\n----- GAME SHOT -----\n\nYou've finished leg with {darts_thrown - 3 + darts_to_finish} darts!\n\nYour average for 3 darts is {avg_score(501, darts_thrown)}\n")
+                    break
+            except Exception:
+                print("\n----- Not valid input ----- \n\nEnter a valid number of darts finished\n\n----------")
+                continue
+        if start_score < 0 or start_score == 1: # score 1 is also bust, due its disability to checkout
+            print(f"\n----- Darts thrown: {darts_thrown} -----\n\n----- BUSTED -----\n\nYou've busted score!\n")
+            start_score += int(main_input)
             continue
-        else:
-            start_score -= int(main_input)
-            if start_score == 0: # needs to be finished
-                darts_thrown += int(input("Darts needed to finish: "))
-            else:
-                darts_thrown += 3
-    except ValueError:
-        print("\n\n----- Not valid input -----\n\nEnter a score or type valid exit\n\n----------\n")
-        continue
-    
-    # check-score logic for each iteration
+        if start_score > 170 and start_score != 0:
+            try:
+                main_input = input("Enter score: ")
+                if main_input in {"quit", "Quit", "exit", "Exit", "QUIT", "EXIT"}:
+                    break
+                if int(main_input) in {163, 166, 169, 172, 173, 175, 176, 178, 179} or int(main_input) > 180:
+                    print(f"\n----- Not valid input -----\n\nEnter a valid score\n\n----------\n")
+                    continue
+                else:
+                    start_score -= int(main_input)
+                    darts_thrown += 3
+                    print(f"\n----- Darts thrown: {darts_thrown} -----\n\nYou've scored: {int(main_input)}\n\nPoints left: {start_score}\n\n----------\n")
+                    continue
+            except ValueError:
+                print("\n----- Not valid input -----\n\nEnter a score (or darts finished) or type valid exit\n\n----------\n")
+                continue
+        if start_score <= 170 and start_score > 0:
+            if start_score in {159, 162, 163, 165, 166, 168, 169}:
+                try:
+                    main_input = input("Enter score: ")
+                    if main_input in {"quit", "Quit", "exit", "Exit", "QUIT", "EXIT"}:
+                        break
+                    if int(main_input) in {163, 166, 169, 172, 173, 175, 176, 178, 179} or int(main_input) > 180:
+                        print(f"\n----- Not valid input -----\n\nEnter a valid score\n\n----------\n")
+                        continue
+                    else:
+                        start_score -= int(main_input)
+                        darts_thrown += 3
+                        print(f"\n----- Darts thrown: {darts_thrown} -----\n\nYou've scored: {int(main_input)}\n\nPoints left: {start_score}\n\n----------\n")
+                        continue
+                except ValueError:
+                    print("\n----- Not valid input -----\n\nEnter a score (or darts finished) or type valid exit\n\n----------\n")
+                    continue
+            try:
+                print(f"----- CHECKOUT -----\n\nYou required {start_score}\n\n----------\n")
+                main_input = input("Enter score: ")
+                if main_input in {"quit", "Quit", "exit", "Exit", "QUIT", "EXIT"}:
+                    break
+                if int(main_input) in {159, 162, 163, 166, 169, 172, 173, 175, 176, 178, 179} or int(main_input) > 180:
+                    print(f"\n\n----- Not valid input -----\n\nEnter a valid score\n\n----------\n")
+                    continue
+                else:
+                    start_score -= int(main_input)
+                    darts_thrown += 3
+            except ValueError:
+                print("\n\n----- Not valid input -----\n\nEnter a score (or darts finished) or type valid exit\n\n----------\n")
+            continue
 
-    if start_score < 0 or start_score == 1: # score 1 is also bust, due its disability to checkout
-        print(f"\n----- Darts thrown: {darts_thrown} -----\n\n----- BUSTED -----\n\nYou've busted score!\n")
-        start_score += int(main_input)
-        print(f"----- CHECKOUT -----\n\nYou required {start_score}\n\n----------\n")
-        continue
-    if start_score > 170:
-        print(f"\n----- Darts thrown: {darts_thrown} -----\n\nYou've scored: {int(main_input)}\n\nPoints left: {start_score}\n\n----------\n")
-    if start_score <= 170 and start_score > 0:
-        print(f"\n----- Darts thrown: {darts_thrown} -----\n\n----- CHECKOUT -----\n\nYou required {start_score}\n\n----------\n")
-    if start_score == 0: 
-        print(f"\n----- GAME SHOT -----\n\nYou've finished leg with {darts_thrown} darts!\n\nYour average for 3 darts is {avg_score(501, darts_thrown)}\n")
-        break
+
+if __name__ == "__main__":
+    main()
